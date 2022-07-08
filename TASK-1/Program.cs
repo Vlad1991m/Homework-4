16fc0a98591efa4a3f13e8536f5705e1ec523a54
Vlad <vlad.melnik@bk.ru>
2022-07-08 23:28:29 +0300

    task-1 added

---
+++ TASK-1/Program.cs
@@ -0,0 +1,20 @@
+﻿int Product(int num)
+{
+    int sum = 0;
+
+    if (num < 0) Console.WriteLine(num = -1);
+    else
+    {
+        while (num > 0)
+        {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        
+    }
+    return sum;
+}
+Console.WriteLine("Input a number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Product(a));
+
